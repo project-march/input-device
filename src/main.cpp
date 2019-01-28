57aed570bc58b15559e783702bcfaa28b51d549c
Roy <r.arriens@gmail.com>
2019-01-28 16:55:53 +0100

    First working blink led

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
-
+#define ledPin 1
+#define led_delay 100 // Delay for a led in ms
 void setup() {
   // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(led_delay);
+  digitalWrite(ledPin,LOW);
+  delay(led_delay);
 }
