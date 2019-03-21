448200b1ae6e88677c758390d3e514ca3bbb2891
Roy Arriens <r.arriens@gmail.com>
2019-03-21 20:16:35 +0100

    moved screen to class

--- lib/uOLED_screen_MARCH3/Screen.cpp
+++ lib/uOLED_screen_MARCH3/Screen.cpp
@@ -1,117 +1,73 @@
-/* * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * *\
- *  Library implementing the software interface for a 4D Systems uOLED-160G2 microOLED  *
- *      GOLDELOX Display.                                                               *
- *  Hardware consists of a screen with a set of ten pins on the back.                   *
- *      Pins "+5V" and "GND" should be connected to their respective voltages.          *
- *      Pin "RES" should be connected to a digital output pin.                          *
- *      Pins "RX" and "TX" should be connected to the pins of a UART peripheral         *
- *          interface. Note that the "RX" pin of the screen should be connected to the  *
- *          "TX" pin of the UART interface, and vice versa.                             *
- *      The other pins should be unconnected.                                           *
- *          ┌────────────────────┐                                                      *
- *          │         3.3V ──O O─┼── RES                                                *
- *          │          GND ──O O─┼── GND                                                *
- *          │          IO1 ──O O─┼── RX                                                 *
- *          │          IO2 ──O O─┼── TX                                                 *
- *          │       5V OUT ──O O─┼── +5V                                                *
- *          │                    │                                                      *
- *          │                    │                                                      *
- *          │                    │                                                      *
- *          └────────────────────┘                                                      *
- *                                                                                      *
- * Created by P. Verton for the MARCH 3 team of Project MARCH                           *
- * Date: 27-MAR-2018                                                                    *
-\* * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * */
-
-// #include "DBS_pindefs.h"
-#include <HardwareSerial.h>
-#include "SoftwareSerial.h"
 #include "Screen.h"
 
 //Constructors
-Screen::Screen(int res, int tx, int rx, int baud)
+Screen::Screen(Goldelox_Serial_4DLib * screen, SoftwareSerial * screenSerial, int rst, int baud, int waitTimeUs)
 {
-    // Serial.println("Setting pins to in/out...");
-    this->m_res = res;
+    // Serial.println("Constructor");
+    
+    // this->m_serial = new SoftwareSerial(rx, tx);
+    // this->m_screen = new Goldelox_Serial_4DLib(m_serial);
+
+    this->m_serial = screenSerial;
+    this->m_screen = screen;
+    this->m_mediaInitialized = 0;
     this->m_baud = baud;
+    this->m_rst = rst;
+    this->m_waitTimeUs = waitTimeUs;
 
-    Serial.println("Creating softwareserial obj...");
-    SoftwareSerial * screenSerial = new SoftwareSerial(rx, tx);
-    // HardwareSerial * screenSerial = new HardwareSerial(0);
-    Serial.println("Starting softwareserial...");
-    screenSerial->begin(m_baud);
+    // Serial.println("Constructor done");
+}
 
-    // Serial.println("Creating Goldelox_serial obj...");
-    this->m_screen = new Goldelox_Serial_4DLib(screenSerial);
-    // this->initialize();
-    Serial.println("Done constructing");
-} //Screen(int res, int tx, int rx, int baud)
+void Screen::initialize(){
 
+    // Serial.println("Initialize");
 
-//Public member functions
-void Screen::reset(){
-    digitalWrite(this->m_res, 0);
-    usleep(10000);
-    digitalWrite(this->m_res, 1);
-    // Serial.println("Sleeping for 3 seconds");
-    sleep(3);
-    // Serial.println("Done sleeping");
-    return;
-}; //reset()
+    // pinMode(m_rst, OUTPUT);
+    digitalWrite(m_rst, 1);
+    // pinMode(m_tx, OUTPUT);
+    // pinMode(m_rx, INPUT);
+    // usleep(m_waitTimeUs);
+
+    // Serial.println("Starting screenserial");
+    m_serial->begin(m_baud);
+    // usleep(m_waitTimeUs);
+
+    // Serial.println("Resetting screen");
+
+    this->reset();
+
+    // Serial.println("Initing media");
+
+    m_mediaInitialized = m_screen->media_Init();
+    usleep(m_waitTimeUs);
+
+    char str[100];
+    sprintf(str, "SD Init status = 0x%02X\n", m_mediaInitialized);
+    m_screen->print(str);
+    usleep(m_waitTimeUs);
+
+    this->clear();
+
+    // Serial.println("Initialization done");
+};
 
 void Screen::clear(){
-    this->m_screen->gfx_Cls();
-    return;
-}; //clear()
+    m_screen->gfx_Cls();
+    usleep(m_waitTimeUs);
+}
 
-void Screen::print(char * string){
-    this->m_screen->gfx_ScreenMode(PORTRAIT_R);
-    this->m_screen->putstr(string);
-    this->m_screen->gfx_ScreenMode(LANDSCAPE);
-    return;
-}; //print(char * string)
+void Screen::reset(){
+    digitalWrite(m_rst, 0);
+    usleep(m_waitTimeUs);
 
-int Screen::display_image_sector(uint32_t sector, int16_t X, int16_t Y){
-    return this->display_image_any(1, sector, X, Y);
+    digitalWrite(m_rst, 1);
+    sleep(3);
 };
 
-int Screen::display_image_byte(uint32_t byte, int16_t X, int16_t Y){
-    return this->display_image_any(0, byte, X, Y);
-};
+void Screen::draw_image(int addr_hi, int addr_lo){
+    m_screen->media_SetSector(addr_hi, addr_lo);
+    // usleep(m_waitTimeUs);
 
-//Private member functions
-void Screen::initialize(){
-    this->m_mediaInitialized = false;
-    Serial.println("Start resetting the screen");
-    this->reset();
-    Serial.println("Start setting screenmode to landscape mode");
-    this->m_screen->gfx_ScreenMode(LANDSCAPE);
-    Serial.println("Changed screenMode to landscape");
-    this->m_screen->gfx_Cls();
-    Serial.println("Cleared Screen");
-    return;
-} //initialize()
-
-int Screen::display_image_any(int sectorMode, uint32_t address, int16_t X, int16_t Y){
-    uint16_t response = 0;
-    if(!this->m_mediaInitialized){
-        response = this->m_screen->media_Init();
-        if(response != 0){
-            this->m_mediaInitialized = true;
-        }
-        else{
-            return -1;
-        }
-    }
-    if(sectorMode){
-        this->m_screen->media_SetSector(address >> 16, address & 0xFFFF);
-    }
-    else{
-        this->m_screen->media_SetAdd(address >> 16, address & 0xFFFF);
-    }
-    if(X || Y){
-        this->m_screen->gfx_Clipping(1);
-    }
-    this->m_screen->media_Image(X, Y);
-    return 0;
-}; //display_image_any(int sectorMode, uint32_t address, int16_t X, int16_t Y)
+    m_screen->media_Image(0,0);
+    usleep(m_waitTimeUs);
+}
--- src/uOLED_screen_example.cpp
+++ src/uOLED_screen_example.cpp
@@ -1,127 +1,70 @@
 #include "Screen.h"
 #include "SoftwareSerial.h"
+#include "SD_sector_addresses.h"
 
-#define UART_TX 32 //software serial only
-#define UART_RX 34 //software serial only
-#define RST 13
+// Developed by M. van der Marel for Project MARCH
+// Required tools for running this code:
+//  Arduino ESP32 board (we use Lolin D32 Pro)
+//  A uOLED-160G2 screen by 4D Systems
+//  The GoldeLox_Serial library for Arduino
+//  An SD card set up with the 4D Systems software with images, placed in the uOLED-160G2 board
+//  The sector addresses returned by the 4D Systems software can be placed in 'SD_sector_addresses.h' for easy access
 
-void screenReset();
-int display_image(Goldelox_Serial_4DLib * display, int sectorMode, uint32_t address, int16_t X, int16_t Y);
+
+#define UART_TX       32 //software serial only
+#define UART_RX       34 //software serial only
+#define RST           13
+#define BAUD_SCREEN   9600
+#define SLEEPTIME_SCREEN  100000
+
 
 SoftwareSerial screenSerial(UART_RX, UART_TX);
-Goldelox_Serial_4DLib display(&screenSerial);
-bool mediaInitialized;
-const int screenSleepTimeUs = 80000;
+Goldelox_Serial_4DLib screenGoldelox(&screenSerial);
+Screen screen(&screenGoldelox, &screenSerial, RST, BAUD_SCREEN, SLEEPTIME_SCREEN);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("setup() started");
+  Serial.println("Setup");
 
   pinMode(RST, OUTPUT);
-  digitalWrite(RST, 1);
+  // digitalWrite(RST, 1);
   pinMode(UART_TX, OUTPUT);
   pinMode(UART_RX, INPUT);
 
-  // SoftwareSerial screenSerial(UART_RX, UART_TX);
-  screenSerial.begin(9600);
-  // Goldelox_Serial_4DLib display(&screenSerial);
-  mediaInitialized = false;
-  // display.screenSleepTimeUsLimit4D = 5000;
+  // screenSerial.begin(BAUD_SCREEN);
 
-  Serial.println("Resetting screen");
-  screenReset();
-//   Serial.println("Setting screen mode to Landscape");
-//   display.gfx_ScreenMode(LANDSCAPE);
-  // display.SSTimeout(0);
-  // display.SSSpeed(0);
-  // display.SSMode(0);
-//   Serial.println("Changing background color to white");
-//   display.gfx_BGcolour(WHITE); //change background color to white
-    // display.gfx_Circle(0,0,20,WHITE);
-  // display.gfx_Cls();            //clear the screen
-  // Serial.println("Printing sector with address 0");
-  // display_image(display, 1, 0x00000000, 0, 0);
+  // digitalWrite(RST, 0);
+  // usleep(SLEEPTIME_SCREEN);
+  // digitalWrite(RST, 1);
+  // sleep(3);
 
-  // Screen screen(RST, UART_TX, UART_RX, 56000);
-  // Serial.println("Starting initialize");
-  // screen.initialize();
-  // display.gfx_ScreenMode(PORTRAIT);
-  // usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
+  // int status = screenGoldelox.media_Init();
+  // usleep(SLEEPTIME_SCREEN);
+  // char str[100];
+  // sprintf(str, "SD Init status = 0x%02X\n", status);
+  // screenGoldelox.print(str);
+  // usleep(SLEEPTIME_SCREEN);
 
-  int status = display.media_Init();
-  usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
-  char str[100];
-  sprintf(str, "SD Init status = 0x%02X\n", status);
-  display.print(str);
-  usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
-  display.gfx_Cls();
-  usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
-  Serial.println("setup() done");
+  // screenGoldelox.gfx_Cls();
+  // usleep(SLEEPTIME_SCREEN);
+
+
+  screen.initialize();
 }
-int i = 0;
+
 void loop(void) {
-  // sprintf(str, "Loop #%i.\n", i++);
-  // Serial.print(str);
-  // display.print(str);
-  // usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
-  // screen.print("Hello wohrld");
-  // sleep(1);
-  // // screen.initialize();
-  // screen.clear();
-  // screen.display_image_sector(0x00000000,0,0);
-  // display.gfx_Cls();
-  // display.print("Hello world in a loop");
-  // sleep(1);
-  display.media_SetSector(0,0);
-  // usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
-  // display.print("Sector set.\n");
-  // usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
-  display.media_Image(0,0);
-  usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
+  Serial.println("Loop");
 
-  display.media_SetSector(0x0000, 0x0051);
-  // usleep(screenSleepTimeUs);
+  // screenGoldelox.media_SetSector(0,0);
+  // screenGoldelox.media_Image(0,0);
+  // usleep(SLEEPTIME_SCREEN);
 
-  display.media_Image(0,0);
-  usleep(screenSleepTimeUs);
+  // screenGoldelox.media_SetSector(0x0000, 0x0051);
+  // screenGoldelox.media_Image(0,0);
+  // usleep(SLEEPTIME_SCREEN);
 
-  // display.gfx_Cls();
-  // usleep(screenSleepTimeUs);  //fucking raar dit. zoek een keer uit ofzo
-  
-//   display.print("Hello world\n");
-//   display.gfx_Circle(i,i,20,WHITE-i);
-sleep(1);
-
-  i++;
+  screen.draw_image(CUP_ADDRESS_HI, CUP_ADDRESS_LO);
+  sleep(1);
+  screen.draw_image(STAIRS_ADDRESS_HI, STAIRS_ADDRESS_LO);
+  sleep(1);
 }
-
-void screenReset(){
-  digitalWrite(RST, 0);
-  usleep(10000);
-  digitalWrite(RST, 1);
-  sleep(3);
-}
-
-int display_image(Goldelox_Serial_4DLib * display, int sectorMode, uint32_t address, int16_t X, int16_t Y){
-    uint16_t response = 0;
-    if(!mediaInitialized){
-        response = display->media_Init();
-        if(response != 0){
-            mediaInitialized = true;
-        }
-        else{
-            return -1;
-        }
-    }
-    if(sectorMode){
-        display->media_SetSector(address >> 16, address & 0xFFFF);
-    }
-    else{
-        display->media_SetAdd(address >> 16, address & 0xFFFF);
-    }
-    if(X || Y){
-        display->gfx_Clipping(1);
-    }
-    display->media_Image(X, Y);
-    return 0;
-};
