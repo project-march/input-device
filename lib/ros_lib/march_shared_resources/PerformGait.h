33d79530ea28b90d837c0ab0775f9d71fa990969
Tim Buckers <timbuckers@gmail.com>
2019-06-04 11:58:49 +0200

    add ros communication

---
+++ lib/ros_lib/march_shared_resources/PerformGait.h
@@ -0,0 +1,92 @@
+#ifndef _ROS_march_shared_resources_PerformGait_h
+#define _ROS_march_shared_resources_PerformGait_h
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ros/msg.h"
+
+namespace march_shared_resources
+{
+
+  class PerformGait : public ros::Msg
+  {
+    public:
+      typedef const char* _gait_name_type;
+      _gait_name_type gait_name;
+      typedef float _time_type;
+      _time_type time;
+      typedef int64_t _repeat_type;
+      _repeat_type repeat;
+
+    PerformGait():
+      gait_name(""),
+      time(0),
+      repeat(0)
+    {
+    }
+
+    virtual int serialize(unsigned char *outbuffer) const
+    {
+      int offset = 0;
+      uint32_t length_gait_name = strlen(this->gait_name);
+      varToArr(outbuffer + offset, length_gait_name);
+      offset += 4;
+      memcpy(outbuffer + offset, this->gait_name, length_gait_name);
+      offset += length_gait_name;
+      offset += serializeAvrFloat64(outbuffer + offset, this->time);
+      union {
+        int64_t real;
+        uint64_t base;
+      } u_repeat;
+      u_repeat.real = this->repeat;
+      *(outbuffer + offset + 0) = (u_repeat.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_repeat.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_repeat.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_repeat.base >> (8 * 3)) & 0xFF;
+      *(outbuffer + offset + 4) = (u_repeat.base >> (8 * 4)) & 0xFF;
+      *(outbuffer + offset + 5) = (u_repeat.base >> (8 * 5)) & 0xFF;
+      *(outbuffer + offset + 6) = (u_repeat.base >> (8 * 6)) & 0xFF;
+      *(outbuffer + offset + 7) = (u_repeat.base >> (8 * 7)) & 0xFF;
+      offset += sizeof(this->repeat);
+      return offset;
+    }
+
+    virtual int deserialize(unsigned char *inbuffer)
+    {
+      int offset = 0;
+      uint32_t length_gait_name;
+      arrToVar(length_gait_name, (inbuffer + offset));
+      offset += 4;
+      for(unsigned int k= offset; k< offset+length_gait_name; ++k){
+          inbuffer[k-1]=inbuffer[k];
+      }
+      inbuffer[offset+length_gait_name-1]=0;
+      this->gait_name = (char *)(inbuffer + offset-1);
+      offset += length_gait_name;
+      offset += deserializeAvrFloat64(inbuffer + offset, &(this->time));
+      union {
+        int64_t real;
+        uint64_t base;
+      } u_repeat;
+      u_repeat.base = 0;
+      u_repeat.base |= ((uint64_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_repeat.base |= ((uint64_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_repeat.base |= ((uint64_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_repeat.base |= ((uint64_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      u_repeat.base |= ((uint64_t) (*(inbuffer + offset + 4))) << (8 * 4);
+      u_repeat.base |= ((uint64_t) (*(inbuffer + offset + 5))) << (8 * 5);
+      u_repeat.base |= ((uint64_t) (*(inbuffer + offset + 6))) << (8 * 6);
+      u_repeat.base |= ((uint64_t) (*(inbuffer + offset + 7))) << (8 * 7);
+      this->repeat = u_repeat.real;
+      offset += sizeof(this->repeat);
+     return offset;
+    }
+
+    const char * getType(){ return "march_shared_resources/PerformGait"; };
+    const char * getMD5(){ return "c2856c4d066eecb97166932dc6907aa4"; };
+
+  };
+
+}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <SoftwareSerial.h>
 #include <Goldelox_Serial_4DLib.h>
 #include <Screen.h>
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int16.h>
+
 
 // Pin definitions
 // Trigger
@@ -20,9 +24,9 @@
 #define JOYSTICK_DOWN     19
 #define JOYSTICK_PUSH     18
 // Screen
-#define UART_TX           32 //software serial
-#define UART_RX           34 //software serial
-#define RST               13
+#define UART_TX           32 // Software serial
+#define UART_RX           34 // Software serial
+#define RST               13 // Reset
 #define BAUD_SCREEN       9600
 
 // Trigger
@@ -40,6 +44,16 @@ Screen screen(&screenGoldelox, &screenSerial, RST, BAUD_SCREEN);
 // State Machine
 StateMachine stateMachine;
 
+// Create ros nodehandle with publishers
+ros::NodeHandle nh;
+std_msgs::String str_msg;
+ros::Publisher scroller_publisher = ros::Publisher("/scroller", &str_msg);
+
+void sendScrollerMessagePress(){
+  str_msg.data = "press";
+  scroller_publisher.publish( &str_msg );
+}
+
 void setup(){
   Serial.begin(9600);
   Serial.println("Prototype");
@@ -52,6 +66,10 @@ void setup(){
   // initialize screen by resetting, initing uSD card, clearing screen
   screen.initialize();
   sleep(1);
+
+  // Initialize ros node for communication.
+  nh.initNode();
+  nh.advertise(scroller_publisher);
 }
 
 void loop(){
@@ -61,10 +79,16 @@ void loop(){
   String joystickPress = joystick.get_press();
   String triggerPress = trigger.read_state();
 
+  if (joystickPress == "PUSH"){
+    sendScrollerMessagePress();
+  }
   // Determine new state
   stateMachine.updateState(joystickState, joystickPress, rockerState, triggerPress);
 
   // Draw appropriate image
   int *drawSdAddresses = stateMachine.getScreenImage();
   screen.draw_image(*(drawSdAddresses), *(drawSdAddresses+1));
+
+  // Spin ros node
+  nh.spinOnce();
 }
