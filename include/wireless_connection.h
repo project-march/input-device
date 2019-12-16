13853db19971b414f388bd19f0856c7b8fdbb256
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-12-16 10:10:30 +0100

    Add clang format to travis

--- include/wireless_connection.h
+++ include/wireless_connection.h
@@ -11,19 +11,17 @@
 const char* ssid = "OnePlus5";
 const char* password = "timisdik";
 
-IPAddress server(192, 168, 43, 83);  // ip of your ROS server
+IPAddress server(192, 168, 43, 83); // ip of your ROS server
 IPAddress ip_address;
 // int status = WL_IDLE_STATUS;
 
 WiFiClient client;
 
-void setupWiFi()
-{
+void setupWiFi() {
   WiFi.begin(ssid, password);
   Serial.print("\nConnecting to ");
   Serial.println(ssid);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print("Could not connect to ");
     Serial.println(ssid);
@@ -34,37 +32,31 @@ void setupWiFi()
   Serial.println(" to access client");
 }
 
-class WiFiHardware
-{
+class WiFiHardware {
 public:
   WiFiHardware(){};
 
-  void init()
-  {
+  void init() {
     // Initialization of TCP server/client setup
     client.connect(server, 11411);
   }
 
   // read a byte from the serial port. -1 = failure
-  int read()
-  {
-    // Read byte from the TCP connection and returns it. returns -1 when not working
+  int read() {
+    // Read byte from the TCP connection and returns it. returns -1 when not
+    // working
     return client.read();
   }
 
   // write data to the connection to ROS
-  void write(uint8_t* data, int length)
-  {
+  void write(uint8_t* data, int length) {
     // write arguments to TCP connection
     for (int i = 0; i < length; i++)
       client.write(data[i]);
   }
 
   // returns milliseconds since start of program
-  unsigned long time()
-  {
-    return millis();
-  }
+  unsigned long time() { return millis(); }
 };
 
-#endif  // WIRELESS_CONNECTION_H
+#endif // WIRELESS_CONNECTION_H
