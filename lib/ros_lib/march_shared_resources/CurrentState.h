d6aad7648f8e1d3dca256ebf5a32f24e7c7e37e4
Olav de Haas <23523462+Olavhaasie@users.noreply.github.com>
2020-08-25 18:41:30 +0200

    Fix CurrentState msg

--- lib/ros_lib/march_shared_resources/CurrentState.h
+++ lib/ros_lib/march_shared_resources/CurrentState.h
@@ -1,103 +1,80 @@
-#ifndef _ROS_SERVICE_CurrentState_h
-#define _ROS_SERVICE_CurrentState_h
+#ifndef _ROS_march_shared_resources_CurrentState_h
+#define _ROS_march_shared_resources_CurrentState_h
+
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
 #include "ros/msg.h"
+#include "std_msgs/Header.h"
 
 namespace march_shared_resources
 {
 
-static const char CURRENTSTATE[] = "march_shared_resources/CurrentState";
-
-  class CurrentStateRequest : public ros::Msg
+  class CurrentState : public ros::Msg
   {
     public:
-
-    CurrentStateRequest()
-    {
-    }
-
-    virtual int serialize(unsigned char *outbuffer) const
-    {
-      int offset = 0;
-      return offset;
-    }
-
-    virtual int deserialize(unsigned char *inbuffer)
-    {
-      int offset = 0;
-     return offset;
-    }
-
-    const char * getType(){ return CURRENTSTATE; };
-    const char * getMD5(){ return "d41d8cd98f00b204e9800998ecf8427e"; };
-
-  };
-
-  class CurrentStateResponse : public ros::Msg
-  {
-    public:
-      typedef const char* _state_type_type;
+      typedef std_msgs::Header _header_type;
+      _header_type header;
+      typedef const char* _state_type;
+      _state_type state;
+      typedef int8_t _state_type_type;
       _state_type_type state_type;
-      typedef const char* _current_state_type;
-      _current_state_type current_state;
+      enum { IDLE =  0 };
+      enum { GAIT =  1 };
 
-    CurrentStateResponse():
-      state_type(""),
-      current_state("")
+    CurrentState():
+      header(),
+      state(""),
+      state_type(0)
     {
     }
 
     virtual int serialize(unsigned char *outbuffer) const
     {
       int offset = 0;
-      uint32_t length_state_type = strlen(this->state_type);
-      varToArr(outbuffer + offset, length_state_type);
+      offset += this->header.serialize(outbuffer + offset);
+      uint32_t length_state = strlen(this->state);
+      varToArr(outbuffer + offset, length_state);
       offset += 4;
-      memcpy(outbuffer + offset, this->state_type, length_state_type);
-      offset += length_state_type;
-      uint32_t length_current_state = strlen(this->current_state);
-      varToArr(outbuffer + offset, length_current_state);
-      offset += 4;
-      memcpy(outbuffer + offset, this->current_state, length_current_state);
-      offset += length_current_state;
+      memcpy(outbuffer + offset, this->state, length_state);
+      offset += length_state;
+      union {
+        int8_t real;
+        uint8_t base;
+      } u_state_type;
+      u_state_type.real = this->state_type;
+      *(outbuffer + offset + 0) = (u_state_type.base >> (8 * 0)) & 0xFF;
+      offset += sizeof(this->state_type);
       return offset;
     }
 
     virtual int deserialize(unsigned char *inbuffer)
     {
       int offset = 0;
-      uint32_t length_state_type;
-      arrToVar(length_state_type, (inbuffer + offset));
+      offset += this->header.deserialize(inbuffer + offset);
+      uint32_t length_state;
+      arrToVar(length_state, (inbuffer + offset));
       offset += 4;
-      for(unsigned int k= offset; k< offset+length_state_type; ++k){
+      for(unsigned int k= offset; k< offset+length_state; ++k){
           inbuffer[k-1]=inbuffer[k];
       }
-      inbuffer[offset+length_state_type-1]=0;
-      this->state_type = (char *)(inbuffer + offset-1);
-      offset += length_state_type;
-      uint32_t length_current_state;
-      arrToVar(length_current_state, (inbuffer + offset));
-      offset += 4;
-      for(unsigned int k= offset; k< offset+length_current_state; ++k){
-          inbuffer[k-1]=inbuffer[k];
-      }
-      inbuffer[offset+length_current_state-1]=0;
-      this->current_state = (char *)(inbuffer + offset-1);
-      offset += length_current_state;
+      inbuffer[offset+length_state-1]=0;
+      this->state = (char *)(inbuffer + offset-1);
+      offset += length_state;
+      union {
+        int8_t real;
+        uint8_t base;
+      } u_state_type;
+      u_state_type.base = 0;
+      u_state_type.base |= ((uint8_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      this->state_type = u_state_type.real;
+      offset += sizeof(this->state_type);
      return offset;
     }
 
-    const char * getType(){ return CURRENTSTATE; };
-    const char * getMD5(){ return "aa90f5901bca2a743ef9c01d50bd9de0"; };
+    const char * getType(){ return "march_shared_resources/CurrentState"; };
+    const char * getMD5(){ return "0fde5ff34496644ac8d85338bbbcbdcd"; };
 
-  };
-
-  class CurrentState {
-    public:
-    typedef CurrentStateRequest Request;
-    typedef CurrentStateResponse Response;
   };
 
 }
